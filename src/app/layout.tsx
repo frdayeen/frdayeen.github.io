04f3c4d95e388fb742554f0604cf0003fbf4062b
Fazle R Dayeen <frdayeen@gmail.com>
2024-01-27 22:56:54 -0600

    top menu

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Fazle R. Dayeen",
+  description: "Profile of Fazle Rabbi Dayeen",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import TopNav from "./components/TopNav";
+
+export default function Home() {
+  return (
+    <main>
+      <TopNav/>
+    </main>
+  );
+}
